dd5ea9aa69577bd12844091354b77fa3b4aefd8f
barakliato@gmail.com <barakliato@gmail.com>
2017-06-12 13:12:48 +0300

    remove class hireracy

--- StringSearcher.cs
+++ StringSearcher.cs
@@ -8,31 +8,28 @@ namespace Algorithems
 {
     public static class StringSearcher
     {
-        public static class RabinKarp
+        private static bool Compare(string prash, string text, int start)
         {
-            private static bool Compare(string prash, string text, int start)
-            {
-                for (int i = 0; i < prash.Length; i++, start++)
-                    if (prash[i] != text[start])
-                        return false;
+            for (int i = 0; i < prash.Length; i++, start++)
+                if (prash[i] != text[start])
+                    return false;
 
-                return true;
+            return true;
+        }
+
+        /// <summary>
+        /// Search for prash in text using Rabin-Karp algorithem
+        /// </summary>
+        /// <returns></returns>
+        public static bool RabinKarpSearch(string text, string prash)
+        {
+            for (int i = 0; i < text.Length - prash.Length; i++)
+            {
+                if (Compare(prash, text, i))
+                    return true;
             }
 
-            /// <summary>
-            /// Search for prash in text using Rabin-Karp algorithem
-            /// </summary>
-            /// <returns></returns>
-            public static bool Search(string text, string prash)
-            {
-                for (int i = 0; i < text.Length - prash.Length; i++)
-                {
-                    if(Compare(prash, text, i))
-                        return true;
-                }
-
-                return false;
-            }
+            return false;
         }
     }
 }
