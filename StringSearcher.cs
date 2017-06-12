0c9639065a1bf0c8cf8334537b9e89d35ad6e26a
barakliato@gmail.com <barakliato@gmail.com>
2017-06-12 14:11:16 +0300

    add boyer moore algorithm - still need to add good suffix table

--- StringSearcher.cs
+++ StringSearcher.cs
@@ -31,5 +31,66 @@ namespace Algorithems
 
             return false;
         }
+
+        public class BoyerMoore
+        {
+            private readonly string _prash;
+            private readonly IDictionary<char, int> _badMatchTable;                     
+
+            /// <summary>
+            /// Boyer Moore algorithm.
+            /// using bad match table and good suffix table
+            /// </summary>
+            /// <param name="prash">The value we are searching</param>
+            public BoyerMoore(string prash)
+            {              
+                _prash = prash;
+                _badMatchTable = new Dictionary<char, int>();
+                for (int i = 0; i < prash.Length - 1; i++)
+                {
+                    var c = prash[i];
+                    var length = prash.Length - i - 1;
+                    if (_badMatchTable.ContainsKey(c))
+                        _badMatchTable[c] = length;
+                    else
+                        _badMatchTable.Add(c, length);                    
+                }
+
+                var last = prash[prash.Length - 1];
+                if (_badMatchTable.ContainsKey(prash[prash.Length - 1]))
+                    _badMatchTable[last] = prash.Length;
+                else
+                    _badMatchTable.Add(last, prash.Length);
+            }
+
+            public bool Search(string text)
+            {
+                var start = _prash.Length - 1;
+                int i = start;
+                while (i < text.Length)
+                {
+                    bool found = true;
+                    for (int j = start; j > -1; j--)
+                    {
+                        var shift = _prash.Length - 1 - j;
+                        var c = text[i - shift];
+                        if (c != _prash[j])
+                        {
+                            i += _badMatchTable.ContainsKey(c)
+                                ? _badMatchTable[c]
+                                : _prash.Length;
+
+                            found = false;
+                            break;
+                        }
+                    }
+
+                    if (found)
+                        return true;
+                }
+
+                return false;
+            }
+        }
     }
 }
